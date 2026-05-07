9c041dade664af68a2bf500cf01d8d5d2cd21932
Josebeb06 <sebastianberrio0612@gmail.com>
2026-05-07 13:36:23 -0500

    Estructura inicial proyecto votación domiciliaria

---
+++ backend/src/main/java/com/votacion/VotacionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.votacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacionBackendApplication.class, args);
+	}
+
+}
